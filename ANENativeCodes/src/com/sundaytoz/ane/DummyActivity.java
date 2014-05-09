fe53f04f43f307c737448ca4a07492112cbaa3b9
jongmin.lee <jongmin.lee@sundaytoz.com>
2014-05-09 12:30:55 +0900

    bitmap 정보를 string 으로 인코딩하는 과정에서 Air 단의 디코딩하는 base64의 설정과 맞추기 위해 인코딩 방법을 Base64.NO_WRAP 으로 변경

--- ANENativeCodes/src/com/sundaytoz/ane/DummyActivity.java
+++ ANENativeCodes/src/com/sundaytoz/ane/DummyActivity.java
@@ -10,79 +10,63 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore.Images;
 import android.util.Base64;
-import android.util.Log;
+import android.view.WindowManager;
 
 public class DummyActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-
-        AndroidExtension.aneContext.dispatchStatusEventAsync("debugging", "onCreate()");
+		
+		// 투명 액티비티 설정
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_BLUR_BEHIND, WindowManager.LayoutParams.FLAG_BLUR_BEHIND);
         
 		Intent intent = new Intent();
         intent.setAction(Intent.ACTION_GET_CONTENT);
         intent.setType("image/*");
 
-
-        AndroidExtension.aneContext.dispatchStatusEventAsync("debugging", "before calling startActivityForResult()");
-
+        // 앨범에서 사진을 받아옴
         startActivityForResult(intent, 0);
-
-        AndroidExtension.aneContext.dispatchStatusEventAsync("debugging", "after calling startActivityForResult()");
 	}
 	
-
+	/**
+	 * 앨범에서 사진을 받아온 후 호출되는 메소드입이다.
+	 */
 	protected void onActivityResult(int requestCode, int resultCode, Intent data)
     {
-    	switch(0)
-    	{
-    	case 0:
-    		try
-    		{    			
-    			AndroidExtension.aneContext.dispatchStatusEventAsync("debugging", "starting startActivityForResult()");
-    			
-    			Uri imgUri = data.getData();
-    			Bitmap bitmap = Images.Media.getBitmap(getContentResolver(), imgUri);
+		try
+		{    			
+			Uri imgUri = data.getData();
+			Bitmap bitmap = Images.Media.getBitmap(getContentResolver(), imgUri);
+			
+			int height = bitmap.getHeight();
+			int width = bitmap.getWidth();
+			
+			// 이미지 사이즈 조정 (높이를 512 이하로 수정)
+			Bitmap resized = null;
+			while(height>512)
+			{
+				resized = Bitmap.createScaledBitmap(bitmap, (width * 512)/height, 512, true);
+				height = resized.getHeight();
+				width = resized.getWidth();
+			}
 
-    			AndroidExtension.aneContext.dispatchStatusEventAsync("debugging", "after getBitmap()");
-
-    			int height = bitmap.getHeight();
-    			int width = bitmap.getWidth();
-    			
-    			Bitmap resized = null;
-    			while(height>512)
-    			{
-    				resized = Bitmap.createScaledBitmap(bitmap, (width * 512)/height, 512, true);
-    				height = resized.getHeight();
-    				width = resized.getWidth();
-    			}
-
-    			AndroidExtension.aneContext.dispatchStatusEventAsync("debugging", "after createScaledBitmap()");
-    			
-    			Log.i("test", String.valueOf(resized.getWidth()));
-    			Log.i("test", String.valueOf(resized.getHeight()));
-
-    			ByteArrayOutputStream stream = new ByteArrayOutputStream();
-    			resized.compress(CompressFormat.JPEG, 100, stream);
-    			byte[] imageByteArray = stream.toByteArray();
-
-    			AndroidExtension.aneContext.dispatchStatusEventAsync("debugging", "after toByteArray()");
-    			    			
-    			String result = Base64.encodeToString(imageByteArray, Base64.DEFAULT);
-    			
-    			AndroidExtension.aneContext.dispatchStatusEventAsync("imageSelected", result);
-    			
-    			finish();
-    		}
-    		catch(Exception e)
-    		{
-    			e.printStackTrace();
-    		}
-    		
-    		break;
-    	}
+			// Bitmap 파일을 String 으로 변환
+			ByteArrayOutputStream stream = new ByteArrayOutputStream();
+			resized.compress(CompressFormat.PNG, 100, stream);
+			byte[] imageByteArray = stream.toByteArray();
+			String encodedString = Base64.encodeToString(imageByteArray, Base64.NO_WRAP);
+			
+			// Air Application 으로 변환한 String 값을 보냄
+			AndroidExtension.aneContext.dispatchStatusEventAsync("imageSelected", encodedString);
+			
+			// 액티비티 종료
+			finish();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
     }
 
 }
--- ANENativeCodes/src/com/sundaytoz/ane/functions/GetImageFunction.java
+++ ANENativeCodes/src/com/sundaytoz/ane/functions/GetImageFunction.java
@@ -1,18 +1,12 @@
 package com.sundaytoz.ane.functions;
 
-import android.content.Intent;
-
 import com.adobe.fre.FREContext;
 import com.adobe.fre.FREFunction;
 import com.adobe.fre.FREObject;
-import com.sundaytoz.ane.AndroidContext;
 import com.sundaytoz.ane.AndroidExtension;
-import com.sundaytoz.ane.DummyActivity;
 
 public class GetImageFunction implements FREFunction {
 
-	private FREContext _ctx;
-	
 	@Override
 	public FREObject call(FREContext arg0, FREObject[] arg1) {
 		
